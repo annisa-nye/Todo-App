ff5a689ebd736a1ca122f779a0bb85d1fb67cfc7
Max Muscat <maxmuscat@Maxs-MacBook-Air.local>
2024-06-24 22:05:32 +1000

    Added Api Data

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -2,45 +2,50 @@ import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { TodoForm } from './TodoForm';
 import { Todo } from './Todo';
+import ApiData from './ApiData';
 
-export const TodoWrapper = () => {
-	const [todos, setTodos] = useState([]);
+const TodoWrapper = () => {
+    const [todos, setTodos] = useState([]);
 
-	const addTodo = (task) => {
-		setTodos([
-			...todos,
-			{ id: uuidv4(), ...task, completed: false, isEditing: false },
-		]);
-	};
+    const addTodo = (task) => {
+        setTodos([
+            ...todos,
+            { id: uuidv4(), ...task, completed: false, isEditing: false },
+        ]);
+    };
 
-	const deleteTodo = (id) => {
-		setTodos(todos.filter((todo) => todo.id !== id));
-	};
+    const deleteTodo = (id) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
 
-	const editTodo = (id, updatedTask) => {
-		setTodos(
-			todos.map((todo) => {
-				if (todo.id === id) {
-					return { ...todo, ...updatedTask, isEditing: false };
-				} else {
-					return todo;
-				}
-			})
-		);
-	};
+    const editTodo = (id, updatedTask) => {
+        setTodos(
+            todos.map((todo) => {
+                if (todo.id === id) {
+                    return { ...todo, ...updatedTask, isEditing: false };
+                } else {
+                    return todo;
+                }
+            })
+        );
+    };
 
-	return (
-		<div className='TodoWrapper'>
-			<h1>✅ To Do Application ✅</h1>
-			<TodoForm addTodo={addTodo} />
-			{todos.map((todo) => (
-				<Todo
-					key={todo.id}
-					task={todo}
-					deleteTodo={deleteTodo}
-					editTodo={editTodo}
-				/>
-			))}
-		</div>
-	);
+    return (
+        <div className='TodoWrapper'>
+            <h1>✅ To Do Application ✅</h1>
+            <TodoForm addTodo={addTodo} />
+            {todos.map((todo) => (
+                <Todo
+                    key={todo.id}
+                    task={todo}
+                    deleteTodo={deleteTodo}
+                    editTodo={editTodo}
+                />
+            ))}
+			<ApiData />
+        </div>
+    );
 };
+
+export default TodoWrapper;
+
